7aabc7289079132e7564def1462d5e18155f80e3
developerrup <developerrup@gmail.com>
2025-08-26 19:11:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,17 @@ import express from "express";
 const app = express();
 const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  res.send(ip);
+  let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+
+  // If multiple IPs (comma-separated), take the first one
+  if (ip.includes(",")) {
+    ip = ip.split(",")[0].trim();
+  }
+  res.status(200).json({ip});
 });
+
 
 app.listen(PORT, () => {
   console.log(`IP service running at http://localhost:${PORT}`);
 });
+
